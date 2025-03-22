eb5d4a3997955970d8ede8b52cdaa7e03998ee9b
Anna <nita-18@mail.ru>
2025-03-22 16:21:28 +0300

    7 Commit

--- python_trening/task_9_oop_1.py
+++ python_trening/task_9_oop_1.py
@@ -1,41 +1,70 @@
-class Input:
-
-    def __init__(self, loc, text):
-        self.loc = loc
-        self.text = text
-
-class Button:
-    def __init__(self, loc, text):
-        self.loc = loc
-        self.text = text
-
-class Title:
-    def __init__(self, loc, text):
-        self.loc = loc
-        self.text = text
-
-class Link:
-    def __init__(self, loc, text):
-        self.loc = loc
-        self.text = text
-
-search = Input('input#search', 'Страница')
-
-print(search.loc)
-print(search.text)
+from task_9_checks import Checks
+class Input(Cheks):
+    def 
 
 
-find = Button('button#find', 'Поиск')
 
-print(find.loc)
-print(find.text)
+    def test13(self):
+        print(check_text)
 
-document = Title('title#document', 'Заголовок')
+test_12 = Input('тест', 'dsj')
 
-print(document.loc)
-print(document.text)
 
-head = Link('link#head', 'Начало')
+from qwe import B
+class A:
+    def __init__(self, _list, value):
+        self._list = _list
+        self.value = value
 
-print(head.loc)
-print(head.text)
+        self.b = B(self._list)
+
+    def qw(self):
+        if self._list[0] == self.value:
+            self.b.calculate()
+
+
+my_list = [1, 1, 2]
+
+a = A(my_list, 1)
+a.qw()
+
+
+#
+# class Button(Cheks):
+#     def __init__(self, loc, text):
+#         self.loc = loc
+#         self.text = text
+#
+# class Title(Cheks):
+#     def __init__(self, loc, text):
+#         self.loc = loc
+#         self.text = text
+#
+# class Link(Cheks):
+#     def __init__(self, loc, text):
+#         self.loc = loc
+#         self.text = text
+
+# find_search = Input()
+# print(find_search.check_text())
+
+# search = Input('input#search', 'Страница')
+#
+# print(search.loc)
+# print(search.text)
+#
+#
+# find = Button('button#find', 'Поиск')
+#
+# print(find.loc)
+# print(find.text)
+#
+# document = Title('title#document', 'Заголовок')
+#
+# print(document.loc)
+# print(document.text)
+#
+# head = Link('link#head', 'Начало')
+#
+# print(head.loc)
+# print(head.text)
